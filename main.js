1f2500cc73841b412d1bf0562da8a6d660bea05b
Cam0411 <89090731+Cam0411@users.noreply.github.com>
2021-12-09 13:39:07 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -10,3 +10,64 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+  
+ // open/close modal when click menu/close
+menu = document.querySelector(".menu")
+modal = document.querySelector(".modal")
+modalClose = document.querySelector(".content__close")
+modalContent = document.querySelector(".content__modal")
+
+function openModal(){
+  modal.classList.add("open")
+  modalContent.classList.add("open2")
+}
+function closeModal(){
+   modal.classList.remove("open")
+   modalContent.classList.remove("open2")
+}
+// add more background
+menu.addEventListener("click",openModal)
+modalClose.addEventListener("click",closeModal)
+
+menu2 = document.querySelector(".menu")
+modal2 = document.querySelector(".modal")
+background = document.querySelector(".background")
+
+function openModal(){
+  modal.classList.add("open")
+  background.classList.add("open2")
+}
+function closeModal(){
+   modal.classList.remove("open")
+   background.classList.remove("open2")
+} 
+menu.addEventListener("click",openModal)
+modalClose.addEventListener("click",closeModal)
+
+// toTOp
+
+
+
+changeColor1 = document.querySelector(".nav__box li:nth-child(2)")
+changeColor2 = document.querySelector(".nav__box li:nth-child(1)")
+changeColor3 = document.querySelector(".nav__box li:nth-child(3)")
+function changebackground(){
+  changeColor1.setAttribute("class", "change__color-3")
+  changeColor2.removeAttribute("class", "change__color-2")
+  changeColor3.removeAttribute("class", "change__color-2")
+}
+function changebackground2(){
+  changeColor2.setAttribute("class", "change__color-2")
+  changeColor1.removeAttribute("class", "change__color-3")
+  changeColor3.removeAttribute("class", "change__color-3")
+}
+function changebackground3(){
+  changeColor3.setAttribute("class", "change__color-2")
+  changeColor2.removeAttribute("class", "change__color-3")
+  changeColor1.removeAttribute("class", "change__color-3")
+}
+changeColor1.addEventListener("click",changebackground)
+changeColor2.addEventListener("click",changebackground2)
+changeColor3.addEventListener("click",changebackground3)
+console.log(changeColor1);
